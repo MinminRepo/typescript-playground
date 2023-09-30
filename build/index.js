f6a7ee59d096a6e5bb64c81af8fb5a0f0c6a700a
Minmin <minmin@localhost.localdomain>
2023-09-30 21:33:04 +0800

    More advanced stuff.

--- build/index.js
+++ build/index.js
@@ -9,4 +9,53 @@ let isThisTrue = false;
 let aListOfNumbers = [
     1, 2, 3, 4, 0.5, 6, 12.3
 ];
+let fillMeUp = [];
+let iAmATuple = [0.05, "Population Growth"];
+var shirtSizes;
+(function (shirtSizes) {
+    shirtSizes[shirtSizes["small"] = 0] = "small";
+    shirtSizes[shirtSizes["medium"] = 1] = "medium";
+    shirtSizes[shirtSizes["large"] = 2] = "large";
+})(shirtSizes || (shirtSizes = {}));
+;
+let myShirtSize = shirtSizes.medium;
+var withString;
+(function (withString) {
+    withString["defaultText"] = "Default";
+    withString[withString["fontSize"] = 12] = "fontSize";
+    withString["defaultColor"] = "black";
+})(withString || (withString = {}));
+;
+let def = withString.defaultText;
+function calculateTax(grossIncome, taxBracket, taxationYear) {
+    if (taxationYear === 2020) {
+        return grossIncome;
+    }
+    else {
+        if (taxBracket > 5) {
+            return grossIncome * 1.15;
+        }
+        else {
+            return grossIncome * 1.20;
+        }
+    }
+}
+console.log(calculateTax(10000, 4, 2019));
+let employee = {
+    id: 123,
+    name: "Marvin",
+    retirementAge: (date) => {
+        return date.getFullYear() + 30;
+    },
+};
+const underpaidEmployee = {
+    id: 3,
+    name: "Bob Stone",
+    retirementAge: (date) => {
+        return date.getFullYear() + 30;
+    },
+};
+let whatAmI = "10";
+whatAmI = 1.4;
+whatAmI = false;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -19,3 +19,129 @@ let aListOfNumbers: number[] = [
 
 /** declaring empty arrays with annotation */
 let fillMeUp: number[] = [];
+
+/** declaring tuples (i don't see a use case for this) */
+let iAmATuple: [number, string] = [0.05, "Population Growth"];
+
+/** declaring enums (might be handy) */
+    // declares the enum.
+    // by declaring zero on the first element, the compiler will automatically
+    // increment the values of the succeeding elements. 
+    enum shirtSizes { small = 0, medium, large };
+    let myShirtSize = shirtSizes.medium; // how to reference the enum and its members
+
+    // below is an example of an enum with strings.
+    // SETTING AN ENUM ELEMENT TO A STRING COMPELS US TO INITIALIZE
+    // EVERY ENUM MEMBER. Surprisingly, errors are emitted when mixing boolean values
+    enum withString { defaultText = "Default", fontSize = 12, defaultColor = "black"};
+    let def = withString.defaultText;
+
+/** declaring and usage of functions 
+ * syntax:
+ * function [Name] ([arg: type], [...]): [return_type] {
+ *  ... body ...
+ * }
+*/
+function calculateTax (grossIncome: number, taxBracket: number, taxationYear: number): number {
+
+    if (taxationYear === 2020) {
+        // hypothetically remove all taxations 
+        // for this year, sanaol.
+        return grossIncome;
+    } else {
+        if (taxBracket > 5) {
+            return grossIncome * 1.15;
+        } else {
+            return grossIncome * 1.20;
+        }
+    }
+
+}
+
+console.log (calculateTax (10000, 4, 2019));
+
+
+/** declaring objects 
+ * the 'readonly' keyword in employee.id declares 
+ * the property to be a constant and cannot be changed
+*/
+let employee: {
+    readonly id: number,
+    name: string,
+    retirementAge: (date: Date) => number
+ } = {
+    id: 123,
+    name: "Marvin",
+    retirementAge: (date: Date) => {
+        return date.getFullYear() + 30;
+    },
+ }
+
+/** declaring obects part 2: type aliasing (like C Structs) 
+ * syntax: 
+ * type [TypeName] = {
+ *  ... properties ...
+ * }
+*/
+type Employee = {
+    readonly id: number,
+    name: string,
+    retirementAge: (date: Date) => number
+};
+
+const underpaidEmployee: Employee = {
+    id: 3,
+    name: "Bob Stone",
+    retirementAge: (date: Date) => {
+        return date.getFullYear() + 30;       
+    },
+}
+
+
+/** union types (you can assign numbers OR others into A SINGLE variable, which defeats the purpose of having TS hahahahaha) 
+ * syntax:
+ *      [variableName]: [type1] | [type2] | [...]
+ * 
+ * can be used in function params too
+*/
+let whatAmI: number | string | boolean = "10";
+whatAmI = 1.4;
+whatAmI = false;
+
+
+/** type intersection
+ * (more like polymorphism of objects in OOP)
+ */
+
+type CanDrag = {
+    isEnabled: boolean,
+    isVisible?: boolean
+};
+
+type CanResize = {
+    resize: (width: number, height: number) => void
+}
+
+type CanBothDragAndResize = CanDrag & CanResize; // syntax
+
+// if a variable with an intersected type is set,
+// all member properties must be initialized, unless
+// explicitly set as optional by using '?' after the property name
+// notice that the 'CanDrag.isVisible' property is not
+// initialized when declaring UIComponent
+let UIComponent: CanBothDragAndResize = {
+    isEnabled: true,
+    resize: (width: number, height: number) => {
+        console.log (width);
+        console.log (height);
+    }
+}
+
+
+
+/** literal types (using actual values as types) 
+ * 
+ * in the below example, the variable 'pickOneValue' can only be set
+ * to either 50 or 100
+*/
+let pickOneValue: 50 | 100 = 100;
