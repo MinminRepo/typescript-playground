3acd5ac25f1800a2c078cadb4ac12e4caf5d4799
Minmin <minmin@localhost.localdomain>
2023-09-30 20:25:59 +0800

    Example variable declarations.

--- build/index.js
+++ build/index.js
@@ -1,4 +1,12 @@
 "use strict";
-console.log("Hello world.");
-let a = 1;
-console.log(a);
+console.log("Hello TS");
+let num1 = 1;
+let float1 = 0.005;
+let double1 = 1234.67883;
+let underscoredNumber = 123456789;
+let aVeryLongString = "A very long string";
+let isThisTrue = false;
+let aListOfNumbers = [
+    1, 2, 3, 4, 0.5, 6, 12.3
+];
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-console.log ("Hello world.");
-let a: number = 1;
-console.log (a);
+console.log ("Hello TS");
+
+/** declare a number (int/float/double) */
+let num1: number = 1;
+let float1: number = 0.005;
+let double1: number = 1234.67883;
+let underscoredNumber: number = 123_456_789;
+
+/** declare strings */
+let aVeryLongString: string = "A very long string";
+
+/** declare boolean values */
+let isThisTrue: boolean = false;
+
+/** declaring arrays of specific type */
+let aListOfNumbers: number[] = [
+    1, 2, 3, 4, 0.5, 6, 12.3
+];
+
+/** declaring empty arrays with annotation */
+let fillMeUp: number[] = [];
